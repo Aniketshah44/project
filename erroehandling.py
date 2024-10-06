1e272665a8517685ec55b0f357729c3d3faf9f62
Aniketshah44 <aniket202006@gmail.com>
2024-10-06 18:09:04 +0530

    project

---
+++ erroehandling.py
@@ -0,0 +1,20 @@
+#create first python project to generate 6 digit random otp
+#wap to convert the $ into rs and vice versa
+#wap to find the age when birthyear given
+
+#error in program
+
+#print(x)
+
+#solution
+ #try:
+#print(x)
+#except NameError:
+print("x is not defined")
+
+#2 error type in python
+
+x="aniket"
+y=2
+c=x+y
+print(c)
